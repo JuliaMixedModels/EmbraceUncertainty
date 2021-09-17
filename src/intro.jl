27350edee33d85a0fb3ea6fd468283f8108842d6
Douglas Bates <dmbates@gmail.com>
2021-09-17 18:56:17 +0200

    More cleanup.  Move data plot to Module

--- src/intro.jl
+++ src/intro.jl
@@ -7,30 +7,22 @@ function dyestufftable()
 end
 
 function dyestuffdataplot()
-    dyestuff = select(    # convert columns to types that Makie knows how to deal with
-        DataFrame(MixedModels.dataset(:dyestuff)),
-        :yield => Array,
-        :batch => PooledArray;
-        renamecols=false,
+    CairoMakie.activate!()
+    dyestuff = DataFrame(MixedModels.dataset(:dyestuff))
+    myld = @combine(groupby(dyestuff, :batch), :mean_yield=mean(:yield))
+    ord = sortperm(myld.mean_yield)
+    obatch = CategoricalArray(dyestuff.batch; levels=myld.batch[ord], ordered=true)
+    axis = (;
+        xlabel="Yield of dyestuff [g]",
+        ylabel="Batch of intermediate product",
+        yticks=(1:6, levels(obatch)),
     )
-    gdf = groupby(dyestuff, :batch)
-    mnyld = @combine(gdf, :mean_yield=mean(:yield))
-    perm = sortperm(mnyld.mean_yield)
-    iperm = invperm(perm)
-    scatter(
-        dyestuff.yield,
-        iperm[dyestuff.batch.refs] .+ randn(30) .* 0.05,
-        axis=(;
-            xlabel="Yield of dyestuff [g]",
-            ylabel="Batch of intermediate product",
-            yticks=(1:6, levels(dyestuff.batch)[perm]),
-        ),
-    )
-    lines!(mnyld.mean_yield[perm], 1:6)
-    Options(current_figure();
-        caption="Yield of dyestuff by batch.  The line joins the mean yields.",
-        label="dyestuffdata",
-    )
+    scatter(Array(dyestuff.yield), obatch.refs; color=(:blue, 0.3), axis)
+    lines!(myld.mean_yield[ord], 1:6)
+    filename = "dyestuff_data"
+    caption="Yield of dyestuff by batch.  The line joins the mean yields by batch."
+    label = "dyestuffdata"
+    Options(current_figure();  filename, caption, label)
 end
 
 function dyestuff2dataplot()
