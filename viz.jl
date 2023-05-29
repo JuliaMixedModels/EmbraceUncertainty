d01c2398ec9a3ff9bca4edfe69576896eaeb36f5
Phillip Alday <me@phillipalday.com>
2023-05-29 00:04:38 -0500

    notes

--- viz.jl
+++ viz.jl
@@ -1,12 +1,3 @@
----
-jupyter: julia-1.9
----
-
-# Vizualization of Mixed Models {#sec-viz}
-
-
-```{julia}
-#| code-fold: true
 const hide_progress = true
 
 using AlgebraOfGraphics
@@ -20,11 +11,6 @@ using MixedModelsMakie
 using ProgressMeter
 using Random
 
-ProgressMeter.ijulia_behavior(:clear);
-CairoMakie.activate!(; type="svg");
-```
-
-```{julia}
 kb07 = DataFrame(MixedModels.dataset("kb07"))
 contrasts = Dict(:item => Grouping(),
                  :subj => Grouping(),
@@ -33,10 +19,6 @@ contrasts = Dict(:item => Grouping(),
                  :load => EffectsCoding())
 
 fm1 = fit(MixedModel, @formula(1000 / rt_raw ~ spkr * prec * load + (1|item) + (1|subj)), kb07; contrasts, progress=true, thin=1)
-```
-
-
-```{julia}
 plot_blups(args...; kwargs...) = plot_blups!(Figure(; resolution=(1000, 600)), args...;kwargs...)
 function plot_blups!(f::Makie.FigureLike, m::MixedModel)
 
@@ -53,21 +35,17 @@ function plot_blups!(f::Makie.FigureLike, m::MixedModel)
     end
     return f
 end
-```
 
-```{julia}
 function eff_dim(m::MixedModel, threshold::Real=0.95)
   re = MixedModels.rePCA(m)
   return NamedTuple{propertynames(re)}(findfirst(>=(threshold), val) for val in values(re))
 end
-```
 
-```{julia}
 fm_max = fit(MixedModel, @formula(1000 / rt_raw ~ spkr * prec * load + (1 + spkr * prec * load |item) + (1 + spkr * prec * load|subj)), kb07; contrasts, progress=true, thin=1)
-```
-
 fm2 = fit(MixedModel, @formula(1000 / rt_raw ~ spkr * prec * load + (1 + prec + load |item) + (1 + prec + load|subj)), kb07; contrasts, progress=true, thin=1)
-
 fm3 = fit(MixedModel, @formula(1000 / rt_raw ~ spkr * prec * load + (1 + prec + load |item) + (1 + prec + load|subj)), kb07; contrasts, progress=true, thin=1)
-
 fm4 = fit(MixedModel, @formula(1000 / rt_raw ~ spkr * prec * load + (1 + prec + load |item) + (1 + load|subj)), kb07; contrasts, progress=true, thin=1)
+
+function plot_objective(m::MixedModel)
+    fitlog = optsum.fitlog
+end
